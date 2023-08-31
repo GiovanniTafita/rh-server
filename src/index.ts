f7b7e36a927a438bac464297d6cbc9b3389a346e
GiovanniTafita <tafitagiovanni0@gmail.com>
2023-08-31 17:08:35 +0300

    init socket

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import dotenv from 'dotenv';
-import { Express } from "express";
 import { serverExpress } from "./app";
 import "reflect-metadata";
 import { initializeDB } from './database/connection';
 import Logger from './logger/logger';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 dotenv.config();
 const port = parseInt(process.env.PORT as string, 10) || 7000;
 const hostname = process.env.HOST || "0.0.0.0";
-const server: Express = serverExpress;
+const httpServer = createServer(serverExpress);
 
-server.listen(port, hostname, () => {
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*'
+  }
+});
+io.on('connection', (socket) => {
+  Logger.info(socket.id);
+  socket.on('test', (data) => {
+    Logger.debug(data);
+  })
+})
+
+httpServer.listen(port, hostname, () => {
   initializeDB();
   Logger.http(`⚡️[server]: Server is running at http://${hostname}:${port}`);
 });
